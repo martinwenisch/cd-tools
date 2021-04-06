2224c6ed849c42065adc6b6a9b85f33428b28f8a
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-04-06 10:32:12 +0200

    Read Airtable API token from env

--- api/rsvp.ts
+++ api/rsvp.ts
@@ -1,5 +1,19 @@
 import { NowRequest, NowResponse } from "@now/node";
+import Airtable from "airtable";
 
 export default async (req: NowRequest, res: NowResponse) => {
-  res.status(500).send("TBD");
+  try {
+    const apiToken = process.env.AIRTABLE_API_KEY;
+    if (!apiToken) {
+      throw "Airtable API key not found in env.";
+    }
+    const table = new Airtable({ apiKey: apiToken }).base("apppZX1QC3fl1RTBM")(
+      "Volunteers"
+    );
+    // TBD: Take record ID, update RSVP column in Airtable
+    res.status(200).send("Všecko bude!");
+  } catch (e) {
+    // TBD: Remove error logging before production deployment
+    res.status(500).send(`Error: ${e}`);
+  }
 };
