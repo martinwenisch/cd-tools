e33e0eee81ca312843b9e67cdc3ac212cc808c40
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-04-06 14:39:59 +0200

    Implement POST to set the RSVP column in Airtable

--- api/rsvp.ts
+++ api/rsvp.ts
@@ -2,7 +2,23 @@ import { NowRequest, NowResponse } from "@now/node";
 import Airtable from "airtable";
 
 export default async (req: NowRequest, res: NowResponse) => {
+  // Only GET or POST supported
+  if (req.method !== "GET" && req.method !== "POST") {
+    res.status(501).send("Not implemented");
+  }
+
+  // GET: return event details
+  if (req.method === "GET") {
+    res.status(501).send("Not implemented yet. Pull requests welcome!");
+    return;
+  }
+
+  // POST: Update RSVP
   try {
+    const recordId = req.body.recordId;
+    if (!recordId) {
+      throw "Required 'recordId' argument missing.";
+    }
     const apiToken = process.env.AIRTABLE_API_KEY;
     if (!apiToken) {
       throw "Airtable API key not found in env.";
@@ -10,8 +26,8 @@ export default async (req: NowRequest, res: NowResponse) => {
     const table = new Airtable({ apiKey: apiToken }).base("apppZX1QC3fl1RTBM")(
       "Volunteers"
     );
-    // TBD: Take record ID, update RSVP column in Airtable
-    res.status(200).send("Všecko bude!");
+    await table.update(recordId, { rsvp_test_zoul: true });
+    res.status(204).send("Resource updated");
   } catch (e) {
     // TBD: Remove error logging before production deployment
     res.status(500).send(`Error: ${e}`);
