490ecef7b3bbd5b3391ab030335428d9af2af0b5
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-04-07 13:56:30 +0200

    Write RSVP info to a separate linked table

--- api/rsvp.ts
+++ api/rsvp.ts
@@ -1,6 +1,11 @@
 import { NowRequest, NowResponse } from "@now/node";
 import Airtable from "airtable";
 
+interface Event {
+  Name: string | undefined;
+  Attendees: string[];
+}
+
 export default async (req: NowRequest, res: NowResponse) => {
   // Only GET or POST supported
   if (req.method !== "GET" && req.method !== "POST") {
@@ -19,14 +24,23 @@ export default async (req: NowRequest, res: NowResponse) => {
     if (!userId) {
       throw "Required 'userId' argument missing.";
     }
+    const eventId = req.body.eventId;
+    if (!eventId) {
+      throw "Required 'eventId' argument missing.";
+    }
     const apiToken = process.env.AIRTABLE_API_KEY;
     if (!apiToken) {
       throw "Airtable API key not found in env.";
     }
     const table = new Airtable({ apiKey: apiToken }).base("apppZX1QC3fl1RTBM")(
-      "Volunteers"
+      "RSVP"
     );
-    await table.update(userId, { rsvp_test_zoul: true });
+    // TBD: Is this a race condition?
+    // https://community.airtable.com/t/append-linked-record-using-api/39420
+    const event = (await table.find(eventId)) as Airtable.Record<Event>;
+    await table.update(eventId, {
+      Attendees: event.fields["Attendees"].concat([userId]),
+    });
     res.status(200).send("Díky, budeme se těšit!");
   } catch (e) {
     // TBD: Remove error logging before production deployment
