14b50102f2956dc0c96066f66a2cacfdbf5108a5
jan stefanides <stefanides@users.noreply.github.com>
2021-09-14 06:15:02 +0200

    nedluzimstatu api: * for cors headers (#11)
    * nedluzimstatu api: new site for cors headers
    
    * nedluzimstatu api: cors respose as '*'

--- api/nedluzimstatu/_shared.ts
+++ api/nedluzimstatu/_shared.ts
@@ -1,11 +1,6 @@
 import * as http from "http";
 import * as express from "express";
 
-const corsAllowedOrigins = [
-  "https://www.nedluzimstatu.cz",
-  "https://staging.nedluzimstatu.cz",
-  "https://nedluzimstatu.ceskodigital.net",
-];
 const corsAllowedHeaders = "Origin, X-Requested-With, Content-Type, Accept";
 const httpCacheControl = "public, max-age=3600";
 
@@ -14,13 +9,7 @@ export function addCORSHeaders(
   request: express.Request,
   _: express.Response
 ) {
-  let origin = request.headers.origin;
-  if (Array.isArray(origin)) {
-    origin = origin[0];
-  }
-  let theOrigin =
-    (typeof origin !== 'udefined' && corsAllowedOrigins.indexOf(origin) >= 0) ? origin : corsAllowedOrigins[0];
-  proxyResponse.headers["Access-Control-Allow-Origin"] = theOrigin;
+  proxyResponse.headers["Access-Control-Allow-Origin"] = '*'; // vercel cache stores reply incl. cors header -> * is required here
   proxyResponse.headers["Access-Control-Allow-Headers"] = corsAllowedHeaders;
   proxyResponse.headers["Cache-Control"] = httpCacheControl;
 }
