9cf1ea756256c4040aeee58db07f0cd483d393f3
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-11-30 08:31:50 +0100

    Revert "Add asynchronous response to deploy Slack bot"
    The asynchronous response mode doesn’t seem to work, perhaps Vercel
    stops executing the serverless func after the response is sent?

--- api/web_deploy.ts
+++ api/web_deploy.ts
@@ -5,35 +5,21 @@ export default async (
   request: VercelRequest,
   response: VercelResponse
 ): Promise<void> => {
-  const deploy_hook = process.env.VERCEL_DEPLOY_HOOK_URL;
-  const response_hook = request.body.response_url as string;
-
-  if (!deploy_hook) {
+  const deploy_url = process.env.VERCEL_DEPLOY_HOOK_URL;
+  if (deploy_url) {
+    const vercel_response = await fetch(deploy_url);
+    if (vercel_response.ok) {
+      response
+        .status(200)
+        .send("Už to frčí, detaily tady: https://cesko.digital/_logs");
+    } else {
+      response.status(500).send("Je to rozbitý, Vercel vrátil chybu :(");
+    }
+  } else {
     response
       .status(500)
       .send(
         "Služba je špatně nastavená, v prostředí chybí proměnná VERCEL_DEPLOY_HOOK_URL."
       );
-    return;
   }
-
-  if (!response_hook) {
-    response.status(400).send("Chybí parametr response_url.");
-    return;
-  }
-
-  response
-    .status(200)
-    .send("Potvrzuju příjem, domlouvám s Vercelem přenasazení webu, moment…");
-
-  const deploy_response = await fetch(deploy_hook);
-  const msg = deploy_response.ok
-    ? "Už to frčí 🥳  Za pár minut by se měla objevit nová verze webu."
-    : "Je to rozbitý, Vercel vrátil chybu :(";
-
-  await fetch(response_hook, {
-    method: "post",
-    body: JSON.stringify({ text: msg }),
-    headers: { "Content-Type": "application/json" },
-  });
 };
