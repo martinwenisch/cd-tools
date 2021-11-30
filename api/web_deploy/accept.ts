e9f3b0721cdf30bef02347fe9533ffe8afc66a11
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-11-30 09:59:55 +0100

    Add async deploy bot version

---
+++ api/web_deploy/accept.ts
@@ -0,0 +1,23 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import fetch from "node-fetch";
+
+export default async (
+  request: VercelRequest,
+  response: VercelResponse
+): Promise<void> => {
+  const response_hook = request.body.response_url as string;
+  if (!response_hook) {
+    response.status(400).send("Chybí parametr response_hook.");
+    return;
+  }
+
+  const params = new URLSearchParams({ response_hook });
+  const root = process.env.VERCEL_URL;
+  await fetch(`https://${root}/api/web_deploy/trigger?${params}`);
+
+  response
+    .status(200)
+    .send(
+      "Potvrzuju příjem, domlouvám s Vercelem přenasazení webu, malý moment…"
+    );
+};
