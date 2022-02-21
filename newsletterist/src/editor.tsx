8dd615554cc3fc99b4195ce153194450a00ba232
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-21 18:07:36 +0100

    Trivial refactoring

--- newsletterist/src/editor.tsx
+++ newsletterist/src/editor.tsx
@@ -1,12 +1,12 @@
 interface Props {
-  onChange?: (text: string) => void;
+  onChange: (text: string) => void;
 }
 
 const Editor: React.FC<Props> = ({ onChange }) => {
   return (
     <textarea
       className="editor"
-      onChange={(event) => onChange && onChange(event.target.value)}
+      onChange={(event) => onChange(event.target.value)}
       placeholder="Sem napiš nebo vlož Markdown."
     />
   );
